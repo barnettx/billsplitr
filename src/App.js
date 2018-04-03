6068be7d965b77450fc278c67bf68539aba36a4c
barnett <barnett>
2018-04-03 21:59:57 +0800

    camera

--- src/App.js
+++ src/App.js
@@ -2,7 +2,23 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Camera from 'react-camera';
+
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.takePicture = this.takePicture.bind(this);
+  }
+
+  takePicture() {
+    this.camera.capture()
+    .then(blob => {
+      this.img.src = URL.createObjectURL(blob);
+      this.img.onload = () => { URL.revokeObjectURL(this.src); }
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -14,9 +30,54 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <h1> HELP </h1>
+
+        <div style={style.container}>
+        <Camera
+          style={style.preview}
+          ref={(cam) => {
+            this.camera = cam;
+          }}
+        >
+          <div style={style.captureContainer} onClick={this.takePicture}>
+            <div style={style.captureButton} />
+          </div>
+        </Camera>
+        <img
+          style={style.captureImage}
+          ref={(img) => {
+            this.img = img;
+          }}
+        />
+      </div>
+
       </div>
     );
   }
 }
+
+const style = {
+  preview: {
+    position: 'relative',
+  },
+  captureContainer: {
+    display: 'flex',
+    position: 'absolute',
+    justifyContent: 'center',
+    zIndex: 1,
+    bottom: 0,
+    width: '100%'
+  },
+  captureButton: {
+    backgroundColor: '#fff',
+    borderRadius: '50%',
+    height: 56,
+    width: 56,
+    color: '#000',
+    margin: 20
+  },
+  captureImage: {
+    width: '100%',
+  }
+};
 
 export default App;
