88a7a31408b543508d1689389c5538d7ee4c56c4
barnettx <barnett.lee.zhi.le@gmail.com>
2018-11-06 11:52:29 +0800

    starting html mockup

--- src/App.js
+++ src/App.js
@@ -1,84 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-import Camera from 'react-camera';
+import CameraScreen from './CameraScreen'
+import ReceiptScreen from './ReceiptScreen'
 
 class App extends Component {
 
-  constructor(props){
-    super(props);
-    this.takePicture = this.takePicture.bind(this);
-  }
-
-  takePicture() {
-    this.camera.capture()
-    .then(blob => {
-      this.img.src = URL.createObjectURL(blob);
-      this.img.onload = () => { URL.revokeObjectURL(this.src); }
-    })
+  constructor(props) {
+    super(props)
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1> cool </h1>
-
-        <div style={style.container}>
-        <Camera
-          style={style.preview}
-          ref={(cam) => {
-            this.camera = cam;
-          }}
-        >
-          <div style={style.captureContainer} onClick={this.takePicture}>
-            <div style={style.captureButton} />
-          </div>
-        </Camera>
-        <img
-          style={style.captureImage}
-          ref={(img) => {
-            this.img = img;
-          }}
-        />
-      </div>
-      <input type="file" accept="image/*" capture="camera" />
-
+        <ReceiptScreen/>
       </div>
     );
   }
 }
 
-const style = {
-  preview: {
-    position: 'relative',
-  },
-  captureContainer: {
-    display: 'flex',
-    position: 'absolute',
-    justifyContent: 'center',
-    zIndex: 1,
-    bottom: 0,
-    width: '100%'
-  },
-  captureButton: {
-    backgroundColor: '#fff',
-    borderRadius: '50%',
-    height: 56,
-    width: 56,
-    color: '#000',
-    margin: 20
-  },
-  captureImage: {
-    width: '100%',
-  }
-};
+
 
 export default App;
